5fd0486de4fac5d915f8b69fbeb2084637c5e7fe
Michal Pokorný <pok@rny.cz>
2015-03-16 13:46:11 +0100

    Remove support for euro account (my money just arrived :)

--- src/github.com/MichalPokorny/worthy/worthy.go
+++ src/github.com/MichalPokorny/worthy/worthy.go
@@ -109,22 +109,8 @@ func GetBitcoinValue() money.Money {
 	return free_currency_converter.Convert(bitcoins, "CZK")
 }
 
-func GetEuroAccountValue() money.Money {
-	path := expand("~/.euro-account")
-	body, err := ioutil.ReadFile(path)
-	if err != nil {
-		panic(err)
-	}
-	amount, err := strconv.ParseFloat(strings.TrimSpace(string(body)), 64)
-	if err != nil {
-		panic(err)
-	}
-	bitcoins := money.New("EUR", amount)
-	return free_currency_converter.Convert(bitcoins, "CZK")
-}
-
 func main() {
-	var mode = flag.String("mode", "", "'broker', 'bitcoin', 'euro_account' or 'table'")
+	var mode = flag.String("mode", "", "'broker', 'broker', 'bitcoin' or 'table'")
 	flag.Parse()
 
 	switch *mode {
@@ -134,14 +120,10 @@ func main() {
 	case "bitcoin":
 		bitcoins := GetBitcoinValue()
 		fmt.Printf("%.2f\n", bitcoins.Amount)
-	case "euro_account":
-		euros := GetEuroAccountValue()
-		fmt.Printf("%.2f\n", euros.Amount)
 	case "table":
 		table := tablewriter.NewWriter(os.Stdout)
 		table.Append([]string{"Bitcoiny", GetBitcoinValue().String()})
 		table.Append([]string{"Akcie", GetBrokerAccountValue().String()})
-		table.Append([]string{"EUR účet", GetEuroAccountValue().String()})
 		table.Render()
 	default:
 		panic("bad mode")
